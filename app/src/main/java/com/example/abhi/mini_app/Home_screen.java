c97c2957c34c319497a9cc1a6d379f0275fa997a
Abhinav <abhinavjha98ald@gmail.com>
2018-09-16 16:50:31 +0530

    Added the drawable menu and changed the icon of app

--- app/src/main/java/com/example/abhi/mini_app/Home_screen.java
+++ app/src/main/java/com/example/abhi/mini_app/Home_screen.java
@@ -2,15 +2,25 @@ package com.example.abhi.mini_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 public class Home_screen extends MainActivity{
     private ImageView ac,tr,st,he;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle at;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        dl =(DrawerLayout)findViewById(R.id.draw);
+        at = new ActionBarDrawerToggle(Home_screen.this,dl,R.string.open,R.string.close);
+        dl.addDrawerListener(at);
+        at.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ac = (ImageView)findViewById(R.id.academics);
         tr =(ImageView)findViewById(R.id.tracking);
         st =(ImageView)findViewById(R.id.stress);
@@ -44,5 +54,13 @@ public class Home_screen extends MainActivity{
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(at.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
